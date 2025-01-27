903bdd23d545215ad7be40ad9ae08464a5577761
R0rt1z2 <me@r0rt1z2.com>
2025-01-27 15:49:52 +0100

    arch/: Import Amazon kernel changes
    * From FireOS 7.6.7.9
    
    Change-Id: I6de21cbfa6530d58a46c0471096e36184c820923
    Signed-off-by: R0rt1z2 <me@r0rt1z2.com>

--- arch/arm/include/uapi/asm/unistd.h
+++ arch/arm/include/uapi/asm/unistd.h
@@ -417,8 +417,6 @@
 #define __NR_userfaultfd		(__NR_SYSCALL_BASE+388)
 #define __NR_membarrier			(__NR_SYSCALL_BASE+389)
 #define __NR_mlock2			(__NR_SYSCALL_BASE+390)
-#define __NR_pidfd_send_signal  (__NR_SYSCALL_BASE+424)
-#define __NR_pidfd_open   		(__NR_SYSCALL_BASE+434)
 
 /*
  * The following SWIs are ARM private.
--- arch/arm/kernel/hw_breakpoint.c
+++ arch/arm/kernel/hw_breakpoint.c
@@ -631,7 +631,7 @@ int arch_validate_hwbkpt_settings(struct perf_event *bp)
 	info->address &= ~alignment_mask;
 	info->ctrl.len <<= offset;
 
-	if (is_default_overflow_handler(bp)) {
+	if (!bp->overflow_handler) {
 		/*
 		 * Mismatch breakpoints are required for single-stepping
 		 * breakpoints.
@@ -686,12 +686,6 @@ static void disable_single_step(struct perf_event *bp)
 	arch_uninstall_hw_breakpoint(bp);
 	counter_arch_bp(bp)->step_ctrl.enabled = 0;
 	arch_install_hw_breakpoint(bp);
-}
-
-static int watchpoint_fault_on_uaccess(struct pt_regs *regs,
-				       struct arch_hw_breakpoint *info)
-{
-	return !user_mode(regs) && info->ctrl.privilege == ARM_BREAKPOINT_USER;
 }
 
 static void watchpoint_handler(unsigned long addr, unsigned int fsr,
@@ -753,15 +747,6 @@ static void watchpoint_handler(unsigned long addr, unsigned int fsr,
 		}
 
 		pr_debug("watchpoint fired: address = 0x%x\n", info->trigger);
-
-		/*
-		 * If we triggered a user watchpoint from a uaccess routine,
-		 * then handle the stepping ourselves since userspace really
-		 * can't help us with this.
-		 */
-		if (watchpoint_fault_on_uaccess(regs, info))
-			enable_single_step(wp, instruction_pointer(regs));
-
 		perf_bp_event(wp, regs);
 
 		/*
@@ -769,8 +754,9 @@ static void watchpoint_handler(unsigned long addr, unsigned int fsr,
 		 * mismatch breakpoint so we can single-step over the
 		 * watchpoint trigger.
 		 */
-		if (is_default_overflow_handler(wp))
+		if (!wp->overflow_handler)
 			enable_single_step(wp, instruction_pointer(regs));
+
 unlock:
 		rcu_read_unlock();
 	}
